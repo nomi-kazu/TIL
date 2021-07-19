66304cc3c43ec5e2c371738a152993502769e761
nomi-kazu <nomikazyu@i.softbank.jp>
2021-07-19 21:04:07 +0900

    fix: registrations_spec

--- api/spec/requests/api/v1/auth/registrations_spec.rb
+++ api/spec/requests/api/v1/auth/registrations_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Auth::Registrations', type: :request do
-  describe "POST /api/v1/auth" do
+  describe 'POST /api/v1/auth' do
     subject { post(api_v1_user_registration_path, params: params) }
-    let(:params) { attributes_for(:user) }
-    it "ユーザー登録できる" do
+
+    let!(:params) { attributes_for(:user) }
+    it 'ユーザー登録できる' do
       subject
       res = JSON.parse(response.body)
-      expect(res["data"]["id"]).to eq(User.last.id)
-      expect(res["data"]["email"]).to eq(User.last.email)
+      expect(res['data']['id']).to eq(User.last.id.to_s)
+      expect(res['data']['attributes']['name']).to eq(User.last.name)
       expect(response).to have_http_status(200)
     end
   end
