17513d381e2004e9848f19a1fc44458fc43eca08
nomi-kazu <nomikazyu@i.softbank.jp>
2021-07-22 00:34:23 +0900

    test: circleci

--- api/config/application.rb
+++ api/config/application.rb
@@ -40,7 +40,7 @@ module App
 
     config.api_only = true
     
-    config.session_store :cookie_store, key: '_interslice_session'
+    # config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies # Required for all session management
     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
     config.middleware.use ActionDispatch::Flash
