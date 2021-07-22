cb207d225bc207404f41c5c786df5a7974f98edf
nomi-kazu <nomikazyu@i.softbank.jp>
2021-07-22 16:54:18 +0900

    feat: Userテーブルにaddressとprofileとusernameカラムを追加 fix: Userテーブルからnicknameカラムを削除

--- api/db/migrate/20210718064742_devise_token_auth_create_users.rb
+++ api/db/migrate/20210718064742_devise_token_auth_create_users.rb
@@ -36,8 +36,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
       # t.datetime :locked_at
 
       ## User Info
-      t.string :name
-      t.string :nickname
+      t.string :name, null: false, limit: 50
       t.string :image
       t.string :email
 
