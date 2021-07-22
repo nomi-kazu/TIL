37ad56a642cb66f29d5321787e23803bf9dfd67b
nomi-kazu <nomikazyu@i.softbank.jp>
2021-07-22 10:16:17 +0900

    refactor: commons.scssを全コンポーネントに反映

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -27,7 +27,7 @@ export default {
    ** Global CSS
    */
   css: [
-    { src: '~/assets/sass/common.scss', lang: 'scss' },
+    { src: '~/assets/sass/commons.scss', lang: 'scss' },
   ],
   /*
    ** Plugins to load before mounting the App
@@ -47,7 +47,7 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/auth',
     ['nuxt-sass-resources-loader', [
-      '~/assets/sass/variable.scss',
+      '~/assets/sass/variables.scss',
     ]],
   ],
   axios: {
