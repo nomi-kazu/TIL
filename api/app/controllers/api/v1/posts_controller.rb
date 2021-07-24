756ffa443bae0db3615ede51bd6f9b1968d9e1e8
nomi-kazu <nomikazyu@i.softbank.jp>
2021-07-24 17:59:12 +0900

    feat: posts_controller.rbを修正

--- api/app/controllers/api/v1/posts_controller.rb
+++ api/app/controllers/api/v1/posts_controller.rb
@@ -1,27 +1,57 @@
-class PostsController < ApplicationController
+class Api::V1::PostsController < ApplicationController
+  before_action :authenticate_api_v1_user!, except: [:show]
+  before_action :set_posts, except: [:new, :create, :show]
 
-  def index
-
+  def show
+    render json: @post, serializer: PostSerializer
   end
 
   def new
-
+    @post = Post.new
+    render json: @post, serializer: PostSerializer
   end
 
   def create
-
+    @post = current_api_v1_user.posts.build(post_params)
+    if @post.save
+      render status: :success, json: @post
+    else
+      render status: error, json: @post.errors
+    end
   end
 
   def edit
-
+    render json: @post, serializer: PostSerializer
   end
 
   def update
-
+    if @post.update(update_post_params)
+      render status: :success, json: @post
+    else
+      render status: error, json: @post.errors
+    end
   end
 
   def destroy
+    if @post.destroy
+      render status: :success, json: @post
+    else
+      render status: error, json: @post.errors
+    end
+  end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :name, :content, :public)
+  end
+
+  def set_posts
+    @post = Post.find(params[:id])
+  end
+
+  def update_post_params
+    params.require(:post).permit(:name, :content, public)
   end
 
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
         sessions: 'api/v1/auth/sessions',
         omniauth_callbacks: 'api/v1/auth/omniauth_callbacks'
       }
-      resources :posts
+      resources :posts, only: [:show, :new, :create, :edit, :update, :destroy]
     end
   end
   
