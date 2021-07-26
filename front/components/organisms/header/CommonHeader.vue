853689b8496c1241d30fdfdd842b8d3b51862598
nomi-kazu <nomikazyu@i.softbank.jp>
2021-07-26 19:37:06 +0900

    feat: header, sidebarを追加

---
+++ front/components/organisms/header/CommonHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- タイトルなど共通なものを書く -->
+    <div>
+      <p>タイトル</p>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/components/organisms/sidebar/SidebarContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- サイドバーを閉じたり開いたりする -->
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
