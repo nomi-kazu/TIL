ebffb22f2120bb59a82c15d9c6ba443ada136f52
nomi-kazu <nomikazyu@i.softbank.jp>
2021-07-27 20:12:54 +0900

    feat: file操作系を追加

--- front/components/templates/UsernameIndexTemplate.vue
+++ front/components/templates/UsernameIndexTemplate.vue
@@ -1,19 +1,29 @@
 <template>
   <two-column-container>
     <template #left>
+      <user-intro-card />
     </template>
 
     <template #right>
+      <h2>あなたのリポジトリ</h2>
+      <folder-breadcrumbs />
+      <file-folder-list-with-action />
     </template>
   </two-column-container>
 </template>
 
 <script>
 import TwoColumnContainer from '~/components/molecules/containers/TwoColumnContainer'
+import FolderBreadcrumbs from '~/components/organisms/breadcrumbs/FolderBreadcrumbs'
+import FileFolderListWithAction from '~/components/organisms/list/FileFolderListWithAction'
+import UserIntroCard from '~/components/organisms/cards/UserIntroCard'
 
 export default {
   components: {
-    TwoColumnContainer
+    TwoColumnContainer,
+    FolderBreadcrumbs,
+    FileFolderListWithAction,
+    UserIntroCard
   }
 }
 </script>
