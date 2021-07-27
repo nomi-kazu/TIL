9a6d710258031d7b0d7a485e92936fe087b1d6d0
nomi-kazu <nomikazyu@i.softbank.jp>
2021-07-27 19:21:05 +0900

    feat: usernameを変更するを追加

--- front/components/templates/SigninEnterusernameTemplate.vue
+++ front/components/templates/SigninEnterusernameTemplate.vue
@@ -1,15 +1,17 @@
 <template>
   <one-column-container>
-
+    <change-username-card />
   </one-column-container>
 </template>
 
 <script>
 import OneColumnContainer from '~/components/molecules/containers/OneColumnContainer'
+import ChangeUsernameCard from '~/organisms/organisms/cards/ChangeUsernameCard'
 
 export default {
   components: {
-    OneColumnContainer
+    OneColumnContainer,
+    ChangeUsernameCard
   }
 }
 </script>
