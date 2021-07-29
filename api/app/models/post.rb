96dc53663898b012c0d575c54461b3d694a39273
nomi-kazu <nomikazyu@i.softbank.jp>
2021-07-29 19:18:56 +0900

    feat: setting

--- api/app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user
-
-  validates :name, presence: true, length: { maximum: 100 }
-  validates :content, presence: true
-  validates :public, inclusion: { in: [true, false] }
-end
