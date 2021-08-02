32d68000e6a4aa3ad75d4d3028a00415e9367ba1
nomi-kazu <nomikazyu@gmail.com>
2021-08-02 22:52:17 +0900

    feat: SigninTemplateの作成

--- front/components/templates/SigninTemplate.vue
+++ front/components/templates/SigninTemplate.vue
@@ -1,6 +1,6 @@
 <template>
   <one-column-container>
-    <h1>ログイン</h1>
+    <h1 class="mb-8">ログイン</h1>
     <signin-card />
   </one-column-container>
 </template>
@@ -18,5 +18,7 @@ export default {
 </script>
 
 <style scoped>
-
+  h1 {
+    color: #646464;
+  }
 </style>
