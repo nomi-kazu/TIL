fc96e8b459a2846c30fb8499a09c9532daebb6bc
nomi-kazu <nomikazyu@gmail.com>
2021-08-06 19:46:10 +0900

    fix: page以外はイベント発火のみするように修正

--- front/components/organisms/cards/SettingProfileCard.vue
+++ front/components/organisms/cards/SettingProfileCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card width="800" class="mx-auto pt-10 pl-6 pb-8">
-    <edit-profile-form :info="info" />
+    <edit-profile-form :info="info" @save="save" />
   </v-card>
 </template>
 
@@ -16,6 +16,12 @@ export default {
     info: {
       type: Object,
       default: undefined
+    }
+  },
+
+  methods: {
+    save(userInfo) {
+      this.$emit('save', userInfo)
     }
   }
 }
