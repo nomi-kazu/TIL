8a55c68bce6260b904089d9cb86eb9dd02f6b343
nomi-kazu <nomikazyu@gmail.com>
2021-09-02 21:14:55 +0900

    feat: 日本語設定

--- back/config/application.rb
+++ back/config/application.rb
@@ -35,6 +35,7 @@ module Til
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.i18n.default_locale = :ja
     config.api_only = true
   end
 end
