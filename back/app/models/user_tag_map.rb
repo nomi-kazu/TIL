fa8aed96b66748cea903b4790ed33024e03ede37
nomi-kazu <nomikazyu@gmail.com>
2021-09-05 11:59:32 +0900

    feat: user_tagテーブルを作成

---
+++ back/app/models/user_tag_map.rb
@@ -0,0 +1,2 @@
+class UserTagMap < ApplicationRecord
+end
--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_025234) do
+ActiveRecord::Schema.define(version: 2021_09_05_025751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,15 @@ ActiveRecord::Schema.define(version: 2021_09_05_025234) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "user_tag_maps", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_user_tag_maps_on_tag_id"
+    t.index ["user_id"], name: "index_user_tag_maps_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -82,4 +91,6 @@ ActiveRecord::Schema.define(version: 2021_09_05_025234) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "post_tag_maps", "posts"
   add_foreign_key "post_tag_maps", "tags"
+  add_foreign_key "user_tag_maps", "tags"
+  add_foreign_key "user_tag_maps", "users"
 end
