edf8570c039fc990c3e7209fdab32d96ea806a94
nomi-kazu <nomikazyu@gmail.com>
2021-09-06 20:52:08 +0900

    feat: Relationsテーブルを作成(follow)

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_025751) do
+ActiveRecord::Schema.define(version: 2021_09_06_114758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,16 @@ ActiveRecord::Schema.define(version: 2021_09_05_025751) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name", limit: 25, null: false
     t.datetime "created_at", precision: 6, null: false
@@ -91,6 +101,8 @@ ActiveRecord::Schema.define(version: 2021_09_05_025751) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "post_tag_maps", "posts"
   add_foreign_key "post_tag_maps", "tags"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
   add_foreign_key "user_tag_maps", "tags"
   add_foreign_key "user_tag_maps", "users"
 end
