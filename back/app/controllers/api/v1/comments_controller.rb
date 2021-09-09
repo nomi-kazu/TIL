a09f1faed99c81d64d9f46832c24a648a4b56200
nomi-kazu <nomikazyu@gmail.com>
2021-09-09 18:25:49 +0900

    fix: comments_controllerを修正

--- back/app/controllers/api/v1/comments_controller.rb
+++ back/app/controllers/api/v1/comments_controller.rb
@@ -2,12 +2,15 @@ module Api
   module V1
     class CommentsController < ApplicationController
       def create
-
+        comment = current_user.comments.new(comment_params)
+        if comment.save
+          render json: comment.as_json(include: [{ user: { methods: :image_url, except: [:email] } }]), status: :created
+        else
+          render json: comment.errors, status: :unprocessable_entity
+        end
       end
 
-      def destroy
-
-      end
+      def destroy; end
 
       private
 
