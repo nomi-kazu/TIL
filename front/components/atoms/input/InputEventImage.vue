5d5875230e271171f7b0ca8dea95bbb32d0745d9
nomi-kazu <nomikazyu@gmail.com>
2021-09-19 14:12:52 +0900

    feat: イベント画像のインプットを作成

---
+++ front/components/atoms/input/InputEventImage.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <v-img
+      :src="eventImageURL ? eventImageURL : '/images/no_img.png'"
+      max-height="200"
+      max-width="300"
+      @click="changeImage"
+    >
+      <div>
+        <v-file-input
+          id="fileInput"
+          v-model="inputValue"
+          accept="image/*"
+          prepend-icon="mdi-plus"
+          hide-input
+          @change="convertImage"
+        />
+      </div>
+    </v-img>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    rules: {
+      type: [Object, String],
+      default: ''
+    },
+
+    value: {
+      type: null,
+      default: ''
+    },
+
+    eventImage: {
+      type: null,
+      default: ''
+    }
+  },
+
+  data () {
+    return {
+      eventImageURL: ''
+    }
+  },
+
+  computed: {
+    inputValue: {
+      get () {
+        return this.value
+      },
+
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  },
+
+  methods: {
+    changeImage () {
+      document.getElementById('fileInput').click()
+    },
+
+    convertImage (file) {
+      if (file !== undefined && file !== null) {
+        if (file.name.lastIndexOf('.') <= 0) {
+          return
+        }
+        const fr = new FileReader()
+        fr.readAsDataURL(file)
+        fr.addEventListener('load', () => {
+          this.eventImageURL = fr.result
+        })
+      } else {
+        this.eventImageURL = ''
+      }
+    }
+  }
+}
+</script>
