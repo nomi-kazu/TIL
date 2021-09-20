1e6282c97ed83948beca319e2e51b785589cad0d
nomi-kazu <nomikazyu@gmail.com>
2021-09-20 11:22:42 +0900

    fix: search_controllerを修正

--- back/app/controllers/api/v1/search_controller.rb
+++ back/app/controllers/api/v1/search_controller.rb
@@ -2,23 +2,27 @@ module Api
   module V1
     class SearchController < ApplicationController
       def users
-        users = User.where('name Like ? OR description Like ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%") if params[:keyword]
-        render json: users
+        users = User.where('name Like ? OR description Like ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+        render json: users.as_json()
       end
 
       def posts
-        posts = Post.where('title Like ? OR content Like ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%") if params[:keyword]
-        render json: posts
+        unless params[:keyword].empty?
+          posts = Post.where('title Like ? OR content Like ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+        else
+          posts = []
+        end
+        render json: posts.as_json(include: [{ user: { methods: :image_url } }, :liked_users, :tags])
       end
 
       def events
-        events = Event.where('title Like ? OR content Like ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%") if params[:keyword]
-        render json: events
+        events = Event.where('title Like ? OR content Like ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%") unless params[:keyword].empty?
+        render json: events.as_json()
       end
 
       def tags
-        tags = Tag.where('name Like ?', "%#{params[:keyword]}%")
-        render json: tags
+        tags = Tag.where('name Like ?', "%#{params[:keyword]}%") unless params[:keyword].empty?
+        render json: tags.as_json()
       end
     end
   end
