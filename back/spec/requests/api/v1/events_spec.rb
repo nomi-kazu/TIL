91eaf43b24b7b5e73d53fdb1ffbbaaec43b1468a
nomi-kazu <nomikazyu@gmail.com>
2021-09-22 18:50:19 +0900

    feat: models/event_specを作成

---
+++ back/spec/requests/api/v1/events_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  let(:event) { create(:event) }
+
+  context 'イベント作成時' do
+    it '各項目が入力済みで成功' do
+      expect(event).to be_valid
+    end
+
+    it '各項目が未入力で失敗' do
+      event.title = ''
+      event.participant_number = ''
+      event.place = ''
+      event.scheduled_date = ''
+      event.start_time = ''
+      event.end_time = ''
+      expect(event).to_not be_valid
+    end
+  end
+
+  context 'イベント更新時' do
+    it 'participant_numberの値が50以下で成功' do
+      event.participant_number = 40
+      expect(event).to be_valid
+    end
+
+    it 'participant_numberの値が50より上で失敗' do
+      event.participant_number = 51
+      expect(event).to_not be_valid
+    end
+  end
+end
