17897205acd46b468de31afba9b0e5a1d6332eb3
nomi-kazu <nomikazyu@gmail.com>
2021-09-22 19:00:08 +0900

    feat: request/event_comments_specを追加

---
+++ back/spec/requests/api/v1/comments_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Comments', type: :request do
+  describe 'GET /destroy' do
+    let!(:user) { create(:user) }
+    let!(:params) { { auth: { email: user.email, password: user.password } } }
+
+    # ログイン処理
+    before do
+      post '/api/v1/user_token', params: params
+    end
+
+    it 'コメントが削除できること' do
+      post = create(:post)
+      comment = create(:comment)
+      delete "/api/v1/comments/#{comment.id}"
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
