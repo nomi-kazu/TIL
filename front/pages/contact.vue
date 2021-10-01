1cc42a1628219202115d09e611fab3f9623730cf
nomi-kazu <nomikazyu@gmail.com>
2021-10-01 18:53:43 +0900

    fix: フッター修正

---
+++ front/pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-card tile>
+      <div>
+        <v-card-title>
+          お問い合わせ
+        </v-card-title>
+        <v-divider />
+      </div>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ front/pages/rules.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-card tile>
+      <div>
+        <v-card-title>
+          利用規約
+        </v-card-title>
+        <v-divider />
+      </div>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
