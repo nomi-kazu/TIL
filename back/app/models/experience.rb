eb2a9b17a94691e65395d91287273f186f937dcd
nomi-kazu <nomikazyu@gmail.com>
2021-10-17 21:54:32 +0900

    feat: exp_rankingを追加

--- back/app/models/experience.rb
+++ back/app/models/experience.rb
@@ -7,4 +7,9 @@ class Experience < ApplicationRecord
   validates :level, presence: true, numericality: { only_integer: true }
   validates :total, presence: true, numericality: { only_integer: true }
   validates :to_next, presence: true, numericality: { only_integer: true }
+
+  def self.lifelong_exp_ranking
+    experiences = Experience.order(lifelong_exp: :desc).limit(10)
+    experiences.map(&:id)
+  end
 end
